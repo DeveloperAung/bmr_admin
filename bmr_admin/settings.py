629039ba0a0e9114d36a9196b989d15a7d905af0
root <root@srv733911.hstgr.cloud>
2025-03-25 06:47:03 +0000

    static file direct fix

--- bmr_admin/settings.py
+++ bmr_admin/settings.py
@@ -259,6 +259,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'bmr_admin/static'),
 ]
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
