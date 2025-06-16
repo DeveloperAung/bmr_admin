4fff89d600ee6fa616117c2becc155f2f3603617
root <root@srv733911.hstgr.cloud>
2025-06-16 05:53:50 +0000

    push server settings

--- bmr_admin/settings.py
+++ bmr_admin/settings.py
@@ -273,7 +273,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'bmr_admin/static'),
 ]
 # FORCE_SCRIPT_NAME = '/bmr_project'
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
